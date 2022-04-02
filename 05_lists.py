d3933495ccdd92871f802ce97ea80ed5780439fd
Robert Vári <mail.robertvari@gmail.com>
2022-04-02 16:39:41 +0200

    update

---
+++ 05_lists.py
@@ -0,0 +1,10 @@
+names = ["Kristóf", "Zoltán", "István", "Tamás", "robert", "kriszta", "csaba"]
+
+
+print(names[-2])
+print(len(names))
+
+# slicing
+print(names[:2])
+print(names[2:4])
+print(names[::2])
