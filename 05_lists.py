fa33921ea54ca9cbfde49b28a343bc8b972a2c8a
Robert Vári <mail.robertvari@gmail.com>
2022-04-02 16:47:51 +0200

    update

--- 05_lists.py
+++ 05_lists.py
@@ -1,10 +1,24 @@
-names = ["Kristóf", "Zoltán", "István", "Tamás", "robert", "kriszta", "csaba"]
+names = ["Kristóf", "Zoltán"]
+
+# add element to the end of the list
+names.append("Csaba")
+
+# insert into index
+names.insert(0, "Kriszta")
+print(names)
+
+names.remove("Zoltán")
+print(names)
+
+del names[-1]
+print(names)
 
 
-print(names[-2])
-print(len(names))
+# print(names[-2])
+# print(len(names))
+#
+# # slicing
+# print(names[:2])
+# print(names[2:4])
+# print(names[::2])
 
-# slicing
-print(names[:2])
-print(names[2:4])
-print(names[::2])
