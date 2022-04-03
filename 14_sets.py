ec75e63788d7f3b682714b421ecff4d0495f7f69
Robert Vári <mail.robertvari@gmail.com>
2022-04-03 15:26:18 +0200

    update

--- 14_sets.py
+++ 14_sets.py
@@ -4,6 +4,10 @@ numbers2 = {3, 4, 5, 6}
 # combine two sets
 print(numbers1 | numbers2)
 
+# remove duplicates from a list
+name_list = ["kriszta", "csaba", "tamás", "csaba", "kriszta"]
+print(list(set(name_list)))
+
 # intersection
 print(numbers1 & numbers2)
 
@@ -19,6 +23,14 @@ print(numbers1)
 
 # add multiple items to a set
 numbers1.update([6, 7, 8, 9])
+print(numbers1)
+
+# remove item from a set
+numbers1.discard(8)
+print(numbers1)
+
+# throws an error if item not in set
+numbers1.remove(9)
 print(numbers1)
 
 # set operators with strings
