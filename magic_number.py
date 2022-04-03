90dfdb8d9df4f746d3349ef9efe3d1639c5e71fe
Robert Vári <mail.robertvari@gmail.com>
2022-04-03 14:22:33 +0200

    update

--- magic_number.py
+++ magic_number.py
@@ -1,16 +1,32 @@
 import random
 
 game_title = "MAGIC NUMBER GAME"
+try_count = 3
 print("-"*50, game_title, "-"*50)
 print("There is a number between 1 and 10. Can you guess it?")
 
+# cast int to string
 magic_number = str(random.randint(1, 10))
+
+# todo remove this from final code
+print(f"MAGIC NUMBER: {magic_number}")
 
 user_guess = input("Your guess?")
 while user_guess != magic_number:
+    try_count -= 1
+
+    # check try_count == 0
+    if try_count == 0:
+        break
+        # game over
+
     print("Wrong guess :( Try again.")
     user_guess = input("Your guess?")
 
-print("You win!")
+# check win condition
+if user_guess == magic_number:
+    print("You win!")
+else:
+    print("You lost. Maybe next time.")
 
 print("-" * (102 + len(game_title)) )
